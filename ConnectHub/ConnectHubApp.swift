4c5faf46bd92cfbfebc85bd0ca16fac3f9023a67
Chris Ferdian <chris.ferdian@gmail.com>
2024-01-05 20:00:31 +0700

    Initial Commit

---
+++ ConnectHub/ConnectHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectHubApp.swift
+//  ConnectHub
+//
+//  Created by Indo Teknologi Utama on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
